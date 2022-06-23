adca08af6494e0c0e269c39df35c716ae3bb9e50
deenaumar <105994665+deenaumar@users.noreply.github.com>
2022-06-23 13:11:34 -0700

    Assgnment

---
+++ ASSIGNMENT 2 CODE 3/ASSIGNMENT 2 CODE 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Exercise2
+{
+    public static void Main()
+    {
+        int j, sum = 0;
+
+        Console.Write("\n\n");
+        Console.Write("Find the sum of first 5 natural numbers:\n");
+        Console.Write("----------------------------------------------");
+        Console.Write("\n\n");
+
+        Console.Write("The first 5 natural number are :\n");
+        for (j = 1; j <= 5; j++)
+        {
+            sum = sum + j;
+            Console.Write("{0} ", j);
+        }
+        Console.Write("\nThe Sum is : {0}\n", sum);
+    }
+}
---
+++ assignment 2/assignment 2/Program.cs
@@ -0,0 +1,26 @@
+﻿//condition.one();
+//condition.two();
+//condition.three();
+//condition.four();
+//condition.five();
+
+//Assignments lopps= new Assignment;
+//loops.one();
+//loops.two();
+//loops.three();
+//loops.four();
+//loops.five ();
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int input = 1;
+        while (input <= 10)
+        {
+            Console.WriteLine(input);
+            input++;
+        }
+    }
+}
